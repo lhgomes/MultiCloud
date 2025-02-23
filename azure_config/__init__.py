e3229d95498d454756f94d07e798a35783b66cad
Luiz Henrique Gomes <lhgomes@live.com>
2025-02-23 12:56:21 +0400

    Update Azure code

--- azure_config/__init__.py
+++ azure_config/__init__.py
@@ -1,6 +1,5 @@
 import azure.functions as func
-from main import unified_handler  # Import your unified handler from main.py
+from .main import unified_handler  # Use a relative import
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Simply forward the request to your unified handler.
     return unified_handler(req)
